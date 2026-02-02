f6aa529dd7ad98d27cfc042d737bc50488b43558
arizkami <amcpethai22@gmail.com>
2026-02-02 15:50:12 +0700

    dcc3699a-d049-429d-9cf3-21290fc1f6c9

--- build.cmd
+++ build.cmd
@@ -1,2 +1,15 @@
 @echo off
-bsd "build.sh"
+setlocal
+
+REM generate GUID
+for /f %%G in ('powershell -NoProfile -Command "[guid]::NewGuid()"') do set GUID=%%G
+
+REM git commit
+git add .
+git commit -m "%GUID%"
+git push
+
+REM run remote build
+bsd sh ~/mochi-src/build.sh
+
+endlocal
